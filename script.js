1de7e8fffd6c12b69cf4bc04ffa387f2b8df994b
Muktar Kabir <mukhtarkabir3394@gmail.com>
2023-09-28 13:34:25 +0100

    Add grid changing functionality

--- script.js
+++ script.js
@@ -4,14 +4,16 @@ const gridContainer = document.getElementById('grid-container');
 function changeBg(e) {
     e.target.classList.add('change');
 }
-for (let i = 0; i < (16*16); i++) {
+
+let rowXColum =16;
+for (let i = 0; i < (rowXColum**2); i++) {
     const boxes = document.createElement('div');
     boxes.classList.add(i+1)
     boxes.classList.add('box')
-    boxes.setAttribute('style','width:30px;height:30px;border-bottom: 0.5px solid black;border-right:0.5px solid black ;') 
+    boxes.setAttribute('style',`width:${480/rowXColum}px;height:${480/rowXColum}px;border-bottom: 0.5px solid black;border-right:0.5px solid black ;`) 
+    boxes.addEventListener('click',changeBg)
     boxes.addEventListener('mouseover',changeBg)
     gridContainer.append(boxes);
-    console.log(boxes);
 }
 
 
