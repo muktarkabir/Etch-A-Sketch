8a26c5091146a0d1579efd037a937d1dbf179ce9
Muktar Kabir <mukhtarkabir3394@gmail.com>
2023-09-28 01:08:06 +0100

    fix changBg function and clean up variables

--- script.js
+++ script.js
@@ -1,13 +1,17 @@
 const gridContainer = document.getElementById('grid-container');
-let boxes;
 
-for (let i = 0; i < 256; i++) {
-    boxes = document.createElement('div');
-    boxes.classList.add(i+1)
-    boxes.setAttribute('style','width:30px;height:30px;border-bottom: 0.5px solid black;border-right:0.5px solid black ;')
-    gridContainer.append(boxes);
 
+function changeBg(e) {
+    e.target.classList.add('change');
 }
-
+for (let i = 0; i < (16*16); i++) {
+    const boxes = document.createElement('div');
+    boxes.classList.add(i+1)
+    boxes.classList.add('box')
+    boxes.setAttribute('style','width:30px;height:30px;border-bottom: 0.5px solid black;border-right:0.5px solid black ;') 
+    boxes.addEventListener('mouseover',changeBg)
+    gridContainer.append(boxes);
+    console.log(boxes);
+}
 
 
