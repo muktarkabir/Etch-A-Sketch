0f09b36d7ae74c0e743d954db68f075369d34d75
Muktar Kabir <mukhtarkabir3394@gmail.com>
2023-10-12 10:27:51 +0100

    Make a 16*16 grid

--- script.js
+++ script.js
@@ -1,19 +1,23 @@
 const gridContainer = document.getElementById('grid-container');
+const newGrid = document.querySelector('.new-grid');
 
 
-function changeBg(e) {
-    e.target.classList.add('change');
+function changeBgtoRed(e) {
+    e.target.classList.add('change-to-red');
 }
 
-let rowXColum =16;
-for (let i = 0; i < (rowXColum**2); i++) {
-    const boxes = document.createElement('div');
-    boxes.classList.add(i+1)
-    boxes.classList.add('box')
-    boxes.setAttribute('style',`width:${480/rowXColum}px;height:${480/rowXColum}px;border-bottom: 0.5px solid black;border-right:0.5px solid black ;`) 
-    boxes.addEventListener('click',changeBg)
-    boxes.addEventListener('mouseover',changeBg)
-    gridContainer.append(boxes);
-}
+let grid;
+grid = 16;
 
 
+for (let i = 0; i < grid**2; i++) {
+        const boxes = document.createElement('div');
+        boxes.classList.add(`box-${i+1}`)
+        boxes.style.width = `${480/grid}px`;
+        boxes.style.height = `${480/grid}px`;
+        boxes.style.borderBottom = `0.5px solid black`;
+        boxes.style.borderRight = `0.5px solid black`;
+        boxes.addEventListener('click',changeBgtoRed);
+        boxes.addEventListener('dragover',changeBgtoRed);
+        gridContainer.append(boxes);
+    }
